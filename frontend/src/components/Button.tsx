3841d6c4096bcbd79119386568c78566f35e1cc6
Heather <heatherheather.tang@mail.utoronto.ca>
2024-10-27 12:37:33 -0400

    Add a dashboard button and test

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button: React.FC = () => {
+    return (<button style={{ color: 'black' , backgroundColor:'green', padding: '10px 20px'}}>
+        Dashboard Button
+        </button>
+    );
+};
+
+export default Button;
