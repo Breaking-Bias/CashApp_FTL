6db05ce8c5367a93abb7a75ce10008b7d466bcc2
Matthew Frieri <mat.frieri@gmail.com>
2024-10-27 15:30:59 -0400

    added graph component and sample input data

--- data/read_data.py
+++ data/read_data.py
@@ -9,7 +9,15 @@ def read_dataset():
 def create_formatted_data():
     raw_data = read_dataset()
 
+    desired_output = [
+        {"2023-07-01": 50}, 
+        {"2023-07-02": 52}, 
+        {"2023-07-03": 55},
+        {"2023-07-04": 54},
+        {"2023-07-03": 57}
+    ]
+
     # Temporary
-    formatted_data = raw_data
+    formatted_data = desired_output
     
     return formatted_data
