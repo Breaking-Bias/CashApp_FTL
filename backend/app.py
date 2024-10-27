acc0eed8c43c138149a44e07592408c31784104c
Alex Magnus <a2005magnus@gmail.com>
2024-10-27 19:43:46 -0400

    added 2 files

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'data'))
-
-from data.arima_test import create_sample_data
-from data.read_data import create_formatted_data
+from data import arima_test, read_data
 
 app = Flask('server')
 CORS(app)
@@ -20,7 +13,7 @@ def getinfo():
 
 @app.route('/getOriginalData')
 def getOriginalData():
-    data = create_formatted_data()
+    data = read_data.create_formatted_data()
     return jsonify(data)
 
 @app.route('/predictValues', methods=['POST'])
@@ -36,7 +29,7 @@ def predictValues():
 
 @app.route('/getDummyData')
 def getDummyData():
-    data = create_sample_data().to_json()
+    data = arima_test.create_sample_data().to_json()
     return data
 
 # This is to test that CI/CD pipeline is working. Delete later.
