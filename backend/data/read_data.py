cbeb49f5709e8fec24bd02ddd9bcf7af8cb7806f
Adventurer-E <erickudou@gmail.com>
2024-10-30 04:26:10 -0400

    Connected the actual data to the endpoint (prediction data not implemented yet)

--- backend/data/read_data.py
+++ backend/data/read_data.py
@@ -1,24 +1,18 @@
 import pandas as pd
+from Model import TARGET_VAR, FORECAST_STEPS, Model
 
 # Read the raw dataset from the csv
 def read_dataset():
     # Temporary
-    return {"data": "yes"}
+    # return {"data": "yes"}
+    return pd.read_csv("transaction_data.csv")
 
 # Creates the data in the format that VISX needs
 def create_formatted_data():
     raw_data = read_dataset()
-
-    desired_output = [
-        {"date": "2023-07-01", "value": 50}, 
-        {"date": "2023-07-02", "value": 52}, 
-        {"date": "2023-07-03", "value": 55}, 
-        {"date": "2023-07-04", "value": 54}, 
-        {"date": "2023-07-05", "value": 57}, 
-
-    ]
-
-    # Temporary
-    formatted_data = desired_output
-    
+    cleaned_data = Model(raw_data).cleaned_data
+    formatted_data = cleaned_data[['Date', TARGET_VAR]].to_dict(orient='records')  # list of dicts
     return formatted_data
+
+def create_prediction_data():
+    ...
