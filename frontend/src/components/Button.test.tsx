1c3e87faced6967d370be04626830adee79beede
taheat04 <heatherheather.tang@mail.utoronto.ca>
2024-10-31 19:26:19 -0400

    Component Button Add Attributes

--- frontend/src/components/Button.test.tsx
+++ frontend/src/components/Button.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';  // Import this to use custom matchers
 import Button from './Button';
 
-test('renders Breaking Bias title', () => {
-    render(<Button />);
-    const butttonComponent = screen.getByText(/Dashboard Button/i);
-    expect(butttonComponent).toBeInTheDocument(); 
+test('ButtonTest', () => {
+    render(<Button backgroundColor="Green" fontSize={8} />);
+    const buttonComponent = screen.getByText(/Dashboard Button/i);
+    expect(buttonComponent).toBeInTheDocument(); 
 });
--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const Button: React.FC = () => {
-    return (<button style={{ color: 'black' , backgroundColor:'green', padding: '10px 20px'}}>
-        Dashboard Button
-        </button>
-    );
+//const Button: React.FC = () => {
+    //return (<button style={{ color: 'black' , backgroundColor:'green', padding: '10px 20px'}}>
+        //Dashboard Button
+        //</button>
+    //);
+//};
+
+type ButtonProps = {
+    backgroundColor : string;
+    fontSize: number;
 };
 
-export default Button;
+export default function Button({
+}: ButtonProps) {
+    return <button> 
+        Dashboard Button 
+    </button>
+}
