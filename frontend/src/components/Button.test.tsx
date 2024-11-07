8e3a52c6cbf0d394a47d4d0a123e9d8fb342368b
taheat04 <heatherheather.tang@mail.utoronto.ca>
2024-11-06 20:08:27 -0500

    Added a test for DropdownFilter

--- frontend/src/components/Button.test.tsx
+++ frontend/src/components/Button.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';  // Import this to use custom matchers
 import Button from './Button';
 
-test('renders Breaking Bias title', () => {
+test('renders Button', () => {
     render(<Button />);
     const butttonComponent = screen.getByText(/Dashboard Button/i);
     expect(butttonComponent).toBeInTheDocument(); 
---
+++ frontend/src/components/DopdownFilter.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import DropdownFilter from "./DropdownFilter";
+import "@testing-library/jest-dom";
+
+test("renders DropdownFilter", () => {
+    render (<DropdownFilter onSelectChange={() => {}} />);
+  
+  // Check if the the Filter is in the document
+  const dropdownElement = screen.getByTitle("Dropdown-Filter");
+  expect(dropdownElement).toBeInTheDocument();
+})
+
+ 
+  // Check if the Filter is working
+
