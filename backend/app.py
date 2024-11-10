0cf12c0be30060aebff47a3035037c751070b5f5
BarbyBard <a2005magnus@gmail.com>
2024-11-09 19:48:26 -0500

    Revert "tara_upload_feature"
    This reverts commit cb03127e32741389461aa217c49874c478fec773.

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import read_data
-import os 
-from werkzeug.utils import secure_filename 
-# import logging
-
-# logging.basicConfig(level=logging.DEBUG)
 
 app = Flask('app')
 CORS(app)
@@ -46,42 +41,6 @@ def predictValuesUnbiased():
     new_values = read_data.create_prediction_data(filtering_factor, forecast_steps, False)
     return jsonify(new_values)
 
-#This is where it starts
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'csv', 'xlsx'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/upload-dataset', methods=['POST'])
-def upload_dataset():
-    if 'file' not in request.files:
-        return jsonify({"message": "No file part"}), 400
-
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"message": "No selected file"}), 400
-
-    # try:
-
-    #     df = pd.read_csv(file)
-        
-    #     data_head = df.head().to_dict(orient='records')
-        
-    #     return jsonify({
-    #         "message": "File uploaded successfully!",
-    #         "data_head": data_head
-    #     })
-    # except Exception as e:
-    #     return jsonify({"message": f"Error processing file: {str(e)}"}), 400
-
- 
-
-#this is where it ends
 
 # This is to test that CI/CD pipeline is working. Delete later.
 @app.route('/cicd_test')
