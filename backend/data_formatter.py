c000dc6a3aff18e573cd30daa0fe203aaaead674
Adventurer-E <erickudou@gmail.com>
2024-11-14 00:04:50 -0500

    successfully enabled prediction on race by adding back missing dates

--- backend/data_formatter.py
+++ backend/data_formatter.py
@@ -141,18 +141,18 @@ class DataFormatter:
         return display_format
 
     @staticmethod
-    def _add_back_missing(df: pd.DataFrame):
+    def _add_back_missing(df: pd.DataFrame) -> pd.DataFrame:
         df.set_index('date', inplace=True)
         all_dates = pd.date_range(start=df.index.min(), end=df.index.max(), freq='D')
         df = df.reindex(all_dates, fill_value=0)
-        df.set_index('date', inplace=True)
+        # df.set_index('date', inplace=True)
         return df
 
     def get_for_predicting(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         """Formats the data for out."""
         amount_df, count_df = self._helper_output_df_format()
 
-        amount_df = self._add_back_missing(amount_df)
-        count_df = self._add_back_missing(count_df)
+        amount_df = DataFormatter._add_back_missing(amount_df)
+        count_df = DataFormatter._add_back_missing(count_df)
 
         return amount_df, count_df
