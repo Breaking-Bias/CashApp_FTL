b16bbaa53ced49f19cf8fd30042777f07bfffbf5
Carlos Solares <carlos.solares@mail.utoronto.ca>
2024-11-16 15:38:58 -0500

    completed graphing_data.py

---
+++ backend/graphing_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# This is the GraphingData class.
+# Only a DataFrame with two columns can be store here.
+# Column one must be labelled "date" and column two must be labelled either "frequency" or "revenue"
+class GraphingData:
+    def __init__(self, data: pd.DataFrame):
+        assert data.shape[1] == 2
+        assert data.columns[1] in ["frequency", "revenue"]
+        self.data = pd.DataFrame
+
+    def getData(self) -> pd.DataFrame:
+        return self.data
+    
