91e9987df4957c547eb7e264fabc95f5969a3eee
Carlos Solares <carlos.solares@mail.utoronto.ca>
2024-11-16 16:43:25 -0500

    resolved pr comments

--- backend/graphing_data.py
+++ backend/graphing_data.py
@@ -6,10 +6,13 @@ import pandas as pd
 # Column 1 must be labelled "date" and column 2 must be
 # labelled either "frequency" or "revenue".
 class GraphingData:
+    data: pd.DataFrame
+
     def __init__(self, data: pd.DataFrame):
-        assert data.shape[1] == 2, "DataFrame must have exactly 2 columns."
-        assert data.columns[1] in ["frequency", "revenue"], "The second column header must be 'frequency' or 'revenue'."
-        self.data = pd.DataFrame
+        assert data.shape[1] == 2, "Data must have exactly 2 columns."
+        assert data.columns[0] == "data", "First column header must be 'data'."
+        assert data.columns[1] in ["frequency", "revenue"], "Second column header must be 'frequency' or 'revenue'."
+        self.data = data
 
     def getData(self) -> pd.DataFrame:
         return self.data
