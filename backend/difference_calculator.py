463b5a112cae9349776e4d2815997ce56ecee4af
taheat04 <heatherheather.tang@mail.utoronto.ca>
2024-11-17 15:31:16 -0500

    Difference_calculator refactoring

--- backend/difference_calculator.py
+++ backend/difference_calculator.py
@@ -1,26 +1,37 @@
 import pandas as pd
+from graphing_data import GraphingData
+
 
 class DifferenceCalculator:
-    # df_unbiased: pd.DataFrame
-    # df_biased: pd.DataFrame
-    #
-    # def __init__(self, file_name: str):
-    #     self.file_name = file_name
-    @staticmethod
-    def _valid_input(df_unbiased: pd.DataFrame, df_biased: pd.DataFrame) -> bool:
-        unbiased_col_name = df_unbiased.columns[1]
-        biased_col_name = df_biased.columns[1]
+    df_unbiased: GraphingData
+    df_biased: GraphingData
+    
+    def __init__(self, df_unbiased: GraphingData, df_biased: GraphingData):
+        self.df_unbiased = df_unbiased.get_data()
+        self.df_biased = df_biased.get_data()
+
+        
+    def _valid_input(self) -> bool:
+        unbiased_col_name = self.df_unbiased.columns[1]
+        biased_col_name = self.df_biased.columns[1]
         return unbiased_col_name == biased_col_name
 
 
-    @staticmethod
-    def volume(df_unbiased: pd.DataFrame, df_biased: pd.DataFrame) -> int:
+    def volume_difference(self) -> int:
         # Expect second (index 1) column to always be the column of interest
-        if not DifferenceCalculator._valid_input(df_unbiased, df_biased):
+        if not self._valid_input():
             raise ValueError("The columns of interest do not match between the"
                              " unbiased and biased dataframes.")
 
-        difference = df_unbiased.iloc[:, 1] - df_biased.iloc[:, 1]
+        difference = self.df_unbiased.iloc[:, 1] - self.df_biased.iloc[:, 1]
         difference = round(difference.sum())
 
         return difference
+    
+
+    def average_difference(self) -> int:
+        difference = self.df_unbiased.iloc[:, 1] - self.df_biased.iloc[:, 1]
+
+        average_diff = difference.mean()
+
+        return int(average_diff)
