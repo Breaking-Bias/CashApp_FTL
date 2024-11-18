74b15165a989315c4442a4ae49ece87eefc07cd4
Matthew Frieri <mat.frieri@gmail.com>
2024-11-17 21:19:40 -0500

    added log

--- frontend/src/pages/HomePage/HomePage.test.tsx
+++ frontend/src/pages/HomePage/HomePage.test.tsx
@@ -3,6 +3,7 @@ import "@testing-library/jest-dom";
 import HomePage from "./HomePage";
 
 test("Renders CashApp Title", () => {
+  console.log("STARTS HERE");
   console.log(process.cwd());
   render(<HomePage />);
   const title = screen.getByText(/Cash App/i);
