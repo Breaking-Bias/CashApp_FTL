a5888766a5faa2c0f6ccc404c77fa9a9bd088983
Adventurer-E <erickudou@gmail.com>
2024-11-21 01:13:40 -0500

    Revert "commit this change to later revert"
    This reverts commit 415f43758cef4911c0d2a699676216504b619b25.

--- backend_test/data_formatter_test.py
+++ backend_test/data_formatter_test.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from graphing_data import GraphingData
+from backend.graphing_data import GraphingData
 from backend.data_formatter import DataFormatter
 from backend.data_reader import DataReader
 
