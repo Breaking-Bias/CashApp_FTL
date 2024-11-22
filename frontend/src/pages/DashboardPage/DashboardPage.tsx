506d28aaa60cd6cd14a537351175cca1baeee66a
avantitandon <157421594+avantitandon@users.noreply.github.com>
2024-11-22 10:18:43 -0500

    took-formatting-logic-out-of-dashboard

--- frontend/src/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage/DashboardPage.tsx
@@ -19,6 +19,7 @@ import InfoTooltip from "./components/InfoTooltip";
 import BigNumber from "./components/BigNumber";
 import HelpModal from "./components/HelpModal"
 import * as React from "react";
+import { formatNumberForDisplay } from "../utils/numberUtils";
 
 export const CYAN = "#00bbbb";
 export const PINK = "#ff4aa4";
@@ -38,26 +39,26 @@ function DashboardPage() {
   const [modeGraphData, setModeGraphData] = useState<OneModeGraphData>();
   const [pastData, setPastData] = useState<FormattedDataEntry[]>();
 
-  function formatNumberForDisplay(num: number): string {
-    if (num >= 1_000_000_000) {
-      const billions = num / 1_000_000_000;
-      return billions >= 100
-        ? `${Math.round(billions)}B`
-        : `${billions.toFixed(1).replace(/\.0$/, "")}B`;
-    } else if (num >= 1_000_000) {
-      const millions = num / 1_000_000;
-      return millions >= 100
-        ? `${Math.round(millions)}M`
-        : `${millions.toFixed(1).replace(/\.0$/, "")}M`;
-    } else if (num >= 1_000) {
-      const thousands = num / 1_000;
-      return thousands >= 100
-        ? `${Math.round(thousands)}K`
-        : `${thousands.toFixed(1).replace(/\.0$/, "")}K`;
-    } else {
-      return num.toString();
-    }
-  }
+  // function formatNumberForDisplay(num: number): string {
+  //   if (num >= 1_000_000_000) {
+  //     const billions = num / 1_000_000_000;
+  //     return billions >= 100
+  //       ? `${Math.round(billions)}B`
+  //       : `${billions.toFixed(1).replace(/\.0$/, "")}B`;
+  //   } else if (num >= 1_000_000) {
+  //     const millions = num / 1_000_000;
+  //     return millions >= 100
+  //       ? `${Math.round(millions)}M`
+  //       : `${millions.toFixed(1).replace(/\.0$/, "")}M`;
+  //   } else if (num >= 1_000) {
+  //     const thousands = num / 1_000;
+  //     return thousands >= 100
+  //       ? `${Math.round(thousands)}K`
+  //       : `${thousands.toFixed(1).replace(/\.0$/, "")}K`;
+  //   } else {
+  //     return num.toString();
+  //   }
+  // }
 
   async function getPastData() {
     const formattedData = await getPastDataAPICall(
