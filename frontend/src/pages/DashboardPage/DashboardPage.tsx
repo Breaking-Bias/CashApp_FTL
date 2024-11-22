e006c289549836a61ef1513c78fb7d896869a9a1
avantitandon <157421594+avantitandon@users.noreply.github.com>
2024-11-22 13:21:18 -0500

    removed-nav-bar-for-now

--- frontend/src/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage/DashboardPage.tsx
@@ -39,26 +39,6 @@ function DashboardPage() {
   const [modeGraphData, setModeGraphData] = useState<OneModeGraphData>();
   const [pastData, setPastData] = useState<FormattedDataEntry[]>();
 
-  // function formatNumberForDisplay(num: number): string {
-  //   if (num >= 1_000_000_000) {
-  //     const billions = num / 1_000_000_000;
-  //     return billions >= 100
-  //       ? `${Math.round(billions)}B`
-  //       : `${billions.toFixed(1).replace(/\.0$/, "")}B`;
-  //   } else if (num >= 1_000_000) {
-  //     const millions = num / 1_000_000;
-  //     return millions >= 100
-  //       ? `${Math.round(millions)}M`
-  //       : `${millions.toFixed(1).replace(/\.0$/, "")}M`;
-  //   } else if (num >= 1_000) {
-  //     const thousands = num / 1_000;
-  //     return thousands >= 100
-  //       ? `${Math.round(thousands)}K`
-  //       : `${thousands.toFixed(1).replace(/\.0$/, "")}K`;
-  //   } else {
-  //     return num.toString();
-  //   }
-  // }
 
   async function getPastData() {
     const formattedData = await getPastDataAPICall(
