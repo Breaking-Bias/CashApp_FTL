3678dc55f600a06a0762272a9c885b9f58f29515
taheat04 <heatherheather.tang@mail.utoronto.ca>
2024-11-23 15:35:21 -0500

    HomePage UI

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -8,13 +8,17 @@ const HomePage = (): JSX.Element => {
   const handleUploadDataset = () => {
     navigate("/upload-dataset"); // Route to upload dataset page
   };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth"});
+  };
   
   return (
-    <div className="container">
+    <div>
+    <div className="initial-section">
       <img alt="Logo" src="/logo.png" />
       <h1>Cash App</h1>
       <h1>Bias Visualization</h1>
-      {/* <img className="rectangle" alt="Background" src={greenBlob} /> */}
       <br />
       {/* Button to go to Upload Dataset */}
       <Button
@@ -28,8 +32,37 @@ const HomePage = (): JSX.Element => {
         Upload Dataset
       </Button>
       <br />
-      {/* Button to go to the graph page */}
+    <div>
+    
     </div>
+        <div className="graph-container">
+          {/* Graph on the left */}
+          <div className="graph">
+            <img alt="Graph Illustration" src="Revenue Gain.png" />
+          </div>
+
+          {/* Paragraph on the right */}
+          <div className="illustration">
+          <p className="goal-text">
+            <strong>Goal:</strong>
+            <br />
+            Provide insights into the potential revenue that could have been earned if specific bias was removed from the dataset. 
+            <br />
+            <span className="highlighted-text">(represented by the shaded area)</span>
+          </p>
+          </div>
+        </div>
+      </div>
+  
+       {/* Get Started Button */}
+       <Button
+        className="get-started-button"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        Go To Top
+      </Button>
+      </div>
+    
   );
 };
 
