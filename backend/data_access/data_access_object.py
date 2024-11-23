21c349dbdd23e3a15e1f7d8c27a746dd92bc1c44
Carlos Solares <carlos.solares@mail.utoronto.ca>
2024-11-23 18:37:22 -0500

    created data_access_object

---
+++ backend/data_access/data_access_object.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import datetime
+
+
+class DataAccessObject:
+    df: pd.DataFrame
+
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def get_data(self) -> pd.DataFrame:
+        return self.df
+
+    def get_column(self, column_name: str) -> pd.Series:
+        return self.df[column_name]
+
+    def get_row(self, index: int) -> pd.Series:
+        return self.df.iloc[index]
+
+    def get_rows(self, start: int, end: int) -> pd.DataFrame:
+        return self.df.iloc[start:end]
+
+    def get_rows_by_date(self, start_date: datetime.date, end_date: datetime.date) -> pd.DataFrame:
+        mask = (self.df['Date'] >= start_date) & (self.df['Date'] <= end_date)
+        return self.df.loc[mask]
+
+    def get_column_by_date(self, column_name: str, start_date: datetime.date, end_date: datetime.date) -> pd.Series:
+        mask = (self.df['Date'] >= start_date) & (self.df['Date'] <= end_date)
+        return self.df.loc[mask, column_name]
--- backend/data_reader.py
+++ backend/data_reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from data_access.data_access_object import DataAccessObject
 
 
 # TODO: May need to change csv file depending on our final data file.
@@ -13,6 +14,8 @@ class DataReader:
         try:
             dataset_path = os.path.join(os.path.dirname(__file__), "data", self.file_name)
             dataset = pd.read_csv(dataset_path)
+            data_access_object = DataAccessObject(dataset)
+            # return data_access_object
             return dataset
         except FileNotFoundError:
             print(f"Error: The file {dataset_path} does not exist.")
