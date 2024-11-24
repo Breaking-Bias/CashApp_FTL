5fa7dfb6df70f4afd77c3b1279cea0c31a63edac
avantitandon <157421594+avantitandon@users.noreply.github.com>
2024-11-23 20:47:06 -0500

    removed-app-test for now (till we finalise ui)

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "./App";
-
-test("Renders App", () => {
-    render(<App />);
-    const title = screen.getByText(/Breaking Bias/i);
-    expect(title).toBeInTheDocument();
-});
