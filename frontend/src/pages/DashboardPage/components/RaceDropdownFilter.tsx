189543ba6203bce125241eeb38dba71ea6834cd3
BarbyBard <a2005magnus@gmail.com>
2024-11-24 23:43:23 -0500

    Updated dropdown filters to have better padding and default option

--- frontend/src/pages/DashboardPage/components/RaceDropdownFilter.tsx
+++ frontend/src/pages/DashboardPage/components/RaceDropdownFilter.tsx
@@ -1,36 +1,42 @@
-import { useState } from "react";
-import { Select, MenuItem, FormControl, InputLabel, SelectChangeEvent } from "@mui/material";
+import {useState} from "react";
+import {
+    Select,
+    MenuItem,
+    FormControl,
+    InputLabel,
+    SelectChangeEvent
+} from "@mui/material";
 
 interface DropdownFilterProps {
-  onSelectChange: (value: string) => void;
+    onSelectChange: (value: string) => void;
 }
 
-function RaceDropdownFilter({ onSelectChange }: DropdownFilterProps) {
-  const [selectedOption, setSelectedOption] = useState("");
+function RaceDropdownFilter({onSelectChange}: DropdownFilterProps) {
+    const [selectedOption, setSelectedOption] = useState("");
 
-  const handleChange = (event: SelectChangeEvent<string>) => {
-    const value = event.target.value;
-    setSelectedOption(value);
-    onSelectChange(value);
-  };
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        const value = event.target.value;
+        setSelectedOption(value);
+        onSelectChange(value);
+    };
 
-  return (
-    <FormControl fullWidth>
-      <InputLabel>Race</InputLabel>
-      <Select
-        value={selectedOption}
-        onChange={handleChange}
-      >
-        <MenuItem value="NoFilter">No Filter</MenuItem>
-        <MenuItem value="Black">Black</MenuItem>
-        <MenuItem value="White">White</MenuItem>
-        <MenuItem value="Asian">Asian</MenuItem>
-        <MenuItem value="Hispanic">Hispanic</MenuItem>
-        <MenuItem value="Mixed">Mixed</MenuItem>
-        <MenuItem value="Other">Other</MenuItem>
-      </Select>
-    </FormControl>
-  );
+    return (
+            <FormControl variant="standard" sx={{ m:1, minWidth: 300 }}>
+                <InputLabel sx={{ml: 2}} >Race</InputLabel>
+                <Select
+                    value={selectedOption}
+                    onChange={handleChange}
+                    variant={"filled"}>
+                    <MenuItem value="">No Filter</MenuItem>
+                    <MenuItem value="Black">Black</MenuItem>
+                    <MenuItem value="White">White</MenuItem>
+                    <MenuItem value="Asian">Asian</MenuItem>
+                    <MenuItem value="Hispanic">Hispanic</MenuItem>
+                    <MenuItem value="Mixed">Mixed</MenuItem>
+                    <MenuItem value="Other">Other</MenuItem>
+                </Select>
+            </FormControl>
+    );
 }
 
 export default RaceDropdownFilter;
