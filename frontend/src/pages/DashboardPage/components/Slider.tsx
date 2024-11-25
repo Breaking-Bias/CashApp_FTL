d9cef87da6da453c27f8eebad0114e8474751304
BarbyBard <a2005magnus@gmail.com>
2024-11-24 23:45:16 -0500

    Added proper help popup to slider to follow ARIA standards

--- frontend/src/pages/DashboardPage/components/Slider.tsx
+++ frontend/src/pages/DashboardPage/components/Slider.tsx
@@ -1,35 +1,43 @@
 import SliderMUI from "@mui/material/Slider";
+import {Tooltip} from "@mui/material";
 
 const MIN_SLIDER_VAL = 10;
 const MAX_SLIDER_VAL = 100;
 export const DEFAULT_SLIDER_VAL = 30;
 
 interface Props {
-  sliderValue: number;
-  setSliderValue: (newValue: number) => void;
+    sliderValue: number;
+    setSliderValue: (newValue: number) => void;
 }
 
-function Slider({ sliderValue, setSliderValue }: Props) {
-  function handleChange(
-    _event: Event,
-    value: number | number[] | null,
-    _activeThumb: number
-  ) {
-    setSliderValue(value as number);
-  }
+function Slider({sliderValue, setSliderValue}: Props) {
+    function handleChange(
+        _event: Event,
+        value: number | number[] | null,
+        _activeThumb: number
+    ) {
+        setSliderValue(value as number);
+    }
 
-  return (
-    <SliderMUI
-      data-testid="forecast-slider"
-      min={MIN_SLIDER_VAL}
-      max={MAX_SLIDER_VAL}
-      aria-label="Default"
-      valueLabelDisplay="auto"
-      value={sliderValue}
-      onChange={handleChange}
-      color="success"
-    />
-  );
+    return (
+        <Tooltip title={
+            <span style={{ fontSize: "14px" }}>
+            Adjust the prediction size, in number of days.
+            </span>}
+                 enterDelay={500}
+                 leaveDelay={200}>
+            <SliderMUI
+                data-testid="forecast-slider"
+                min={MIN_SLIDER_VAL}
+                max={MAX_SLIDER_VAL}
+                aria-label="Default"
+                valueLabelDisplay="auto"
+                value={sliderValue}
+                onChange={handleChange}
+                color="success"
+            />
+        </Tooltip>
+    );
 }
 
 export default Slider;
