00fe90fcfe9651c816280e12a8b1deff12fd81e7
Carlos Solares <carlos.solares@mail.utoronto.ca>
2024-11-26 16:52:45 -0500

    changes to prediction_controller.py

--- backend/interface_adaptor/prediction/prediction_controller.py
+++ backend/interface_adaptor/prediction/prediction_controller.py
@@ -1,4 +1,4 @@
-from use_case.prediction.prediction_interactor import PredictionInteractor
+from backend.use_case.prediction.old_prediction_interactor import PredictionInteractor
 from use_case.interactor_helpers.filter_interactor import FilterInteractor
 import pandas as pd
 
@@ -14,14 +14,29 @@ class PredictionController:
         self.original_dataset = original_dataset
         self.filter_gender = filter_gender
         self.filter_race = filter_race
-        self.filtered_dataset = self.original_dataset.filter()
         self.forecast_steps = forecast_steps
+        self.filtered_biased_dataset = self.original_dataset.copy().filter_biased()
+        self.filtered_unbiased_dataset = self.original_dataset.copy().filter_unbiased()
         self.prediction_interactor = PredictionInteractor(self.filtered_dataset, self.forecast_steps)
 
-    def filter(self):
-        filtered_dataset = FilterInteractor(self.original_dataset).filter_by(self.filter_gender, self.filter_race)
-        return filtered_dataset
+    def filter_biased(self) -> pd.DataFrame:
+        filtered_biased_dataset = (FilterInteractor(self.original_dataset)
+                                .filter_by(self.filter_gender, self.filter_race)
+                                .filter_invalid_transactions())
+        return filtered_biased_dataset
 
-    def execute(self):
+    def filter_unbiased(self) -> pd.DataFrame:
+        filtered_unbiased_dataset = (FilterInteractor(self.original_dataset)
+                                .filter_by(self.filter_gender, self.filter_race)
+                                .unbias()
+                                .filter_invalid_transactions())
+        return filtered_unbiased_dataset
+
+    def execute(self) -> dict[str, dict[str, list[dict]]]:
+        """Returns a dictionary containing:
+        {"revenue_graph": revenue_graph, "frequency_graph": frequency_graph}
+
+        Note that revenue_graph and frequency_graph are the formatted
+        """
         return self.prediction_interactor.make_prediction()
     
