2c341777aa385420fa878a37203789345eeb2d3a
taheat04 <heatherheather.tang@mail.utoronto.ca>
2024-12-03 13:35:08 -0500

    Netlify remove unused imports

--- frontend/src/pages/DashboardPage/DashboardPage.test.tsx
+++ frontend/src/pages/DashboardPage/DashboardPage.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import DashboardPage from './DashboardPage';
 import { getGraphDataAPICall, getPastDataAPICall } from '../../services/ApiCalls';
 import '@testing-library/jest-dom';
--- frontend/src/services/ExportServices.test.tsx
+++ frontend/src/services/ExportServices.test.tsx
@@ -1,7 +1,4 @@
-import { render, screen } from '@testing-library/react';
 import { ExportService } from './ExportService';  // Adjust the import path to match your actual file structure
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 
 // Mock jsPDF and html2canvas
 jest.mock('jspdf', () => {
@@ -14,33 +11,8 @@ jest.mock('jspdf', () => {
 jest.mock('html2canvas', () => jest.fn());
 
 // Example component to test the export functionality
-const GraphComponent = () => (
-  <div id="graph-id">
-    <canvas></canvas>
-  </div>
-);
 
 describe('ExportService', () => {
-//   it('should export the graph to a PDF when the element exists', async () => {
-    
-//     // Render the GraphComponent to the document body
-//     render(<GraphComponent />);
-
-//     // Check if the element exists in the document
-//     const graphElement = screen.getByTestId('graph-id');
-//     expect(graphElement).toBeInTheDocument();
-
-//     // Call the export function
-//     ExportService.exportGraphToPDF('graph-id');
-
-//     // Wait for the async code to finish (since html2canvas is async)
-//     await Promise.resolve();
-
-//     // Check if jsPDF methods were called
-//     const doc = new jsPDF();
-//     expect(doc.addImage).toHaveBeenCalledWith('data:image/png;base64,someimage', 'PNG', 10, 10, 180, 160);
-//     expect(doc.save).toHaveBeenCalledWith('graph.pdf');
-//   });
 
   it('should alert if the graph element is not found', () => {
     // Mock the alert method
@@ -53,17 +25,4 @@ describe('ExportService', () => {
     expect(alertMock).toHaveBeenCalledWith('Graph not found!');
   });
 
-//   it('should call html2canvas with the correct element', async () => {
-    
-//     // Render the GraphComponent to the document body
-//     render(<GraphComponent />);
-
-//     // Call export function
-//     ExportService.exportGraphToPDF('graph-id');
-//     await Promise.resolve();
-
-//     // Check if html2canvas was called with the correct element
-//     const graphElement = screen.getByTestId('graph-id');
-//     expect(html2canvas).toHaveBeenCalledWith(graphElement);
-//   });
 });
